8a438e647483932e113b3bac5c6943b128d7186d
Valerio <spagnoli.1973484@studenti.uniroma1.it>
2023-04-24 13:07:35 +0200

    aggiornata pagina noleggio sicronizzata con database

--- res/var/db.php
+++ res/var/db.php
@@ -15,6 +15,28 @@
                     cognome VARCHAR(32) NOT NULL
                     );";
 
-    $queries = array($create_utente);
+    $create_auto = "CREATE TABLE IF NOT EXISTS auto(
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    modello VARCHAR(32) NOT NULL,
+                    marca VARCHAR(32) NOT NULL,
+                    colore VARCHAR(32) NOT NULL,
+                    cambio ENUM ('automatico', 'manuale') NOT NULL, 
+                    carburante ENUM ('benzina', 'diesel', 'ibrido', 'gpl', 'elettrico') NOT NULL,
+                    prezzo_giornaliero DECIMAL NOT NULL,
+                    percorso_img VARCHAR(100) NOT NULL
+                    );";
 
+    $create_noleggio = "CREATE TABLE IF NOT EXISTS noleggio(
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        id_auto INT NOT NULL REFERENCES auto(id),
+                        id_utente INT NOT NULL REFERENCES utente(id), 
+                        data_inizio DATE NOT NULL, 
+                        data_fine DATE NOT NULL, 
+                        prezzo_tot DECIMAL NOT NULL, 
+                        stato ENUM ('in_corso', 'completato', 'annullato')
+                        );";
+
+    $queries = array($create_utente, $create_auto, $create_noleggio);
+
+    
 ?>
---
+++ res/var/sql_noleggio.php
@@ -0,0 +1,5 @@
+<?php
+	
+	$auto_noleggio = "SELECT * FROM auto";
+	
+?>
