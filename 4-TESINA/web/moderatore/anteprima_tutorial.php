435c5a1e749c74e2b4aad8a79bbbddcae0732c78
danielesiciliano <128803104+danielesiciliano@users.noreply.github.com>
2024-02-03 18:29:43 +0100

    mod

--- 4-TESINA/web/moderatore/anteprima_tutorial.php
+++ 4-TESINA/web/moderatore/anteprima_tutorial.php
@@ -10,12 +10,7 @@ $id_bozza = $_SESSION['id_bozza'];
 $tutorial_bozza = getTutorialBozza($tps_root, $id_bozza);
 $path = "index.php"; 
 $mod = "moderatore";     
-addressing($_SESSION['Tipo_utente'], $mod, $path);
 $steps = $tutorial_bozza->childNodes;
-
-$logout = $tps_root . "lib/logout.php?ban=true";
-addressing($_SESSION['ban'], 0, $logout);
-
 if($steps->length < 1){
   header('Location: form_step.php');
   exit;
@@ -28,6 +23,11 @@ if(isset($_GET['num_step'])){
 }
 
 $step = $steps->item($num_step);
+addressing($_SESSION['Tipo_utente'], $mod, $path);
+
+$logout = $tps_root . "lib/logout.php?ban=true";
+addressing($_SESSION['ban'], 0, $logout);
+
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -43,7 +43,6 @@ $step = $steps->item($num_step);
       <link type="text/css" rel="stylesheet" href="../../res/css/standard/progetti.css" />
       <link type="text/css" rel="stylesheet" href="../../res/css/anteprima_tutorial.css" />
 
-      
 
   </head>
 
@@ -67,7 +66,7 @@ $step = $steps->item($num_step);
       <div class="bar"></div>
         <div class="toolbar"></div>
 
-        <div class="title-add-tutorial"><h2>AGGIUNGI TUTORIAL</h2></div>
+        <div class="title-add-tutorial"><h2>AGGIUNGI TUTORIAL<h2></div>
         <div id="error">
                 <?php 
                     if(isset($_SESSION['empty_form']) && $_SESSION['empty_form'] === "true" ){
@@ -95,9 +94,9 @@ $step = $steps->item($num_step);
                 $descrizione_step = $step->getElementsByTagName('descrizione')->item(0)->nodeValue;
                 $titolo_step = $step->getAttribute('titolo_step');
 
-                  echo "<div class=\"anteprima_box\">";
+                echo "<div class=\"anteprima_box\">";
           
-                  echo "<div class=\"insert_before\">";
+                  echo"<div class=\"insert_before\">";
                       echo "<form class=\"insert_form\" method=\"post\" action=\"form_step.php\">";
                         if($num_step-1 < 0){
                           $before = 0;
@@ -107,23 +106,23 @@ $step = $steps->item($num_step);
                         echo "<input type=\"hidden\" name=\"num_step\" value=\"$before\"></input>";
                         echo "<button type=\"submit\">AGGIUNGI PRIMA</button>";
                       echo "</form>";
-                  echo "</div>"; //chiusura insert_before
+                  echo"</div>"; //chiusura insert_before
 
-                  echo "<div class=\"step step-anteprima\">\n"; //a
-                  echo "    <div class=\"step-container\">\n"; //b
-                  echo "        <div class=\"step-content\">\n"; //c
+                  echo "<div class=\"step step-anteprima\">\n";
+                  echo "    <div class=\"step-container\">\n";
+                  echo "        <div class=\"step-content\">\n";
                   echo "            <div class=\"step-img\" style=\"background-image: url('../../$img_path'); background-size: cover; background-position: center;\"></div>\n";
-                  echo "            <div class=\"descrizione\">\n"; //d
+                  echo "            <div class=\"descrizione\">\n";
                   echo "                <div class=\"fase\"><h4>".$titolo_step." - STEP " . $num_step+1 . "</h4></div>\n";
                   echo "                <div class=\"testo\">$descrizione_step</div>\n";
-                  echo "            </div>\n"; //d
-                  echo "        </div>\n"; //c
-                  echo "        <form action=\"" . $tps_root . "lib/forward_numstep.php?anteprima=true\" method=\"post\">\n";//e
-                  echo "          <input type=\"hidden\" value=\"$num_step\" name=\"num_step\"></input>\n";
-                  echo "          <div class=\"move-button\">\n";//f
+                  echo "            </div>\n";
+                  echo "        </div>\n";
+                  echo "        <form action=\"" . $tps_root . "lib/forward_numstep.php?anteprima=true\" method=\"post\">\n";
+                  echo "        <input type=\"hidden\" value=\"$num_step\" name=\"num_step\"></input>\n";
+                  echo "        <div class=\"move-button\">\n";
 
                   if($num_step == 0){
-                      echo "            <div class=\"left\"></div>\n";
+                      echo "            <div class=\"left\" type=\"submit\"></div>\n";
                   } else{
                       echo "            <button class=\"left l\" name=\"action\" value=\"prev\" type=\"submit\">&#129184; Prev</button>\n";
                   }
@@ -131,15 +130,15 @@ $step = $steps->item($num_step);
                   if($step->nextSibling){
                       echo "            <button class=\"right r\" name=\"action\" value=\"next\" type=\"submit\">Next &#129185;</button>\n";
                   } else{
-                      echo "            <div class=\"right\"></div>\n";
+                      echo "            <div class=\"right\" type=\"submit\"></div>\n";
                   }
 
-                  echo "          </div>\n";//f
-                  echo "        </form>\n";//e
-                  echo " </div>\n";//b
-                  echo " </div>\n";//a
+                  echo "        </div>\n";
+                  echo "        </form>\n";
+                  echo " </div>";
+                  echo " </div>";
 
-                  echo"<div class=\"insert_after\">\n";
+                  echo"<div class=\"insert_after\">";
                     echo "<form class=\"insert_form\" method=\"post\" action=\"form_step.php\">";
                         echo "<input type=\"hidden\" name=\"num_step\" value=\"". $num_step+1 ."\"></input>";
                         echo "<button type=\"submit\">AGGIUNGI DOPO</button>";
@@ -148,8 +147,6 @@ $step = $steps->item($num_step);
 
                 echo "</div>"; //chiusura anteprima_box
     ?>
-
-
 
 
 <?php
